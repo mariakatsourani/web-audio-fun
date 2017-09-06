f0caf87b4dbc7f5a4bbd5c862b3230ab704655d6
Maria Katsourani <maria.katsourani@chas.se>
2017-09-06 15:51:54 +0200

    Add change handler for volume slider

--- src/Volume.js
+++ src/Volume.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
 
 class Volume extends Component {
+  constructor() {
+    super();
+    
+    this.state = {value: ''}
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({value: e.target.value});
+  }
+
 
   render() {
     return (
-        <div className="left">
-            <span>Volume:</span>
-            <input type="range" min="0.0" max="1.0" step="0.01" value="0.5" list="volumes" name="volume" />
-            <datalist id="volumes">
-                <option value="0.0" label="Mute"/>
-                <option value="1.0" label="100%"/>
-            </datalist>
-        </div>
+      <div className="left">
+        <span>Volume:</span>
+        <input onChange={this.handleChange} type="range" min="0.0" max="1.0" step="0.01" value="0.5" list="volumes" name="volume" />
+        <p>Current volume:{this.state.value}</p>
+        <datalist id="volumes">
+          <option value="0.0" label="Mute" />
+          <option value="1.0" label="100%" />
+        </datalist>
+      </div>
     );
   }
 }
